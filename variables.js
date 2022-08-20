bad89856200def394043a09cef82f47460185ab9
Virginia <virgicardoso@gmail.com>
2022-08-19 23:12:21 -0300

    prueba

---
+++ variables.js
@@ -0,0 +1,15 @@
+// variables? tipo de dato?
+//nombres de las variables(estandares)
+//valores de las variables
+//Distintas maneras de declarar variables, que usamos??
+//alcance variables -> bloques de codigos
+//operadores matematicos
+//operadores comparación
+//operadores p/ booleanos
+
+
+
+
+
+
+
